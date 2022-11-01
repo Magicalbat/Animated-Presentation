3639f0cc312a374eb058d702159882d8ddd6a7eb
Magicalbat <magicalbat@comcast.net>
2022-11-01 17:10:37 -0400

    Seems to work on linux :)

--- Animated-Presentation/src/main.c
+++ Animated-Presentation/src/main.c
@@ -1,53 +1,31 @@
 #include <stdio.h>
 
-#include "base/base.h"
+#include "base/base.h" #include "os.h"
 
 int main() {
-	// Page size of my pc is 4096
-    
     printf("program init\n");
 
-	arena_t* arena = arena_create(4096);
+    getwchar();
 
-    printf("arena made\n");
+    uint8_t* buff = os_mem_reserve(4096 * 1000);
 
-	string8_t str = string8_create(arena, 128);
-	memset(str.str, 'a', str.len);
-	printf("%s\n", str.str);
-    
-    printf("string made\n");
+    printf("mem reserved\n");
+    getwchar();
 
-	arena_free(arena);
-    
-    printf("arena free\n");
+    os_mem_commit(buff, 4096 * 500);
+
+    printf("mem commited\n");
+    getwchar();
+
+    os_mem_decommit(buff, 4096 * 500);
+
+    printf("mem decommited\n");
+    getwchar();
+
+    os_mem_release(buff, 4096 * 1000);
+
+    printf("mem released\n");
+    getwchar();
 	
 	return 0;
 }
-
-
-/*
-#ifdef AP_PLATFORM_LINUX
-
-#include <sys/mman.h>
-
-#include "os.h"
-
-//https://stackoverflow.com/questions/2782628/any-way-to-reserve-but-not-commit-memory-in-linux
-//https://www.ibm.com/docs/en/i/7.2?topic=ssw_ibm_i_72/apis/mmap.html
-
-void* os_mem_reserve(uint64_t size) {
-	void* out = mmap(0, size, PROT_NONE, MAP_ANONYMOUS, -1, 0);
-	return out;
-}
-void os_mem_commit(void* ptr, uint64_t size) {
-    mprotect(ptr, size, PROT_READ | PROT_WRITE);
-}
-void os_mem_decommit(void* ptr, uint64_t size) {
-    mprotect(ptr, size, PROT_NONE);
-}
-void os_mem_release(void* ptr, uint64_t size) {
-    munmap(ptr, size);
-}
-
-#endif 
-*/
